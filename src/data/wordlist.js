e67e0de3d900c68d07eedd213795074832db19b2
Flashy <thomas.el.8258@outlook.com>
2023-12-11 10:55:08 +0000

    added data

--- src/data/wordlist.js
+++ src/data/wordlist.js
@@ -1,35 +1,71 @@
-let words = 
-[Cat, Dog, Boy, Bus, Bee, Cow, Dad, Egg, Fox, Hat, Mum, Car, "Key", "Pig", "Ear", "Owl"]
-[{
-    word: "Cat",
-    image: "cat.jpeg"
-},
-{
-    word: "Dog",
-    image: "cat.jpeg"
-},
+export const words = [
+	{
+		word: "Cat",
+		image: "cat.jpeg",
+	},
+	{
+		word: "Dog",
+		image: "cat.jpeg",
+	},
 
-{
-    word: "Boy",
-    image: "cat.jpeg"
-}
+	{
+		word: "Boy",
+		image: "boy.jpeg",
+	},
+	{
+		word: "Bus",
+		image: "bus.jpeg",
+	},
 
-[{
-    word: "Bus",
-    image: "cat.jpeg"
-}],
+	{
+		word: "Bee",
+		image: "bee.jpeg",
+	},
 
-[{
-    word: "Bee",
-    image: "cat.jpeg"
-}],
+	{
+		word: "Cow",
+		image: "cow.jpeg",
+	},
+	{
+		word: "Dad",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Egg",
+		image: "egg.jpeg",
+	},
+	{
+		word: "Fox",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Hat",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Mum",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Car",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Key",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Pig",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Ear",
+		image: "dad.jpeg",
+	},
+	{
+		word: "Owl",
+		image: "dad.jpeg",
+	},
+];
 
-{
-    word: "Cat",
-    image: "cat.jpeg"
-},
-{
-    word: "Cat",
-    image: "cat.jpeg"
-}
-
+// [Cat, Dog, Boy, Bus, Bee, Cow, Dad, Egg, Fox, Hat, Mum, Car, "Key", "Pig", "Ear", "Owl"]
