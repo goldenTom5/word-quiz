a97fdfc205bf5546ea2d3b3ef1c6c971c9960446
Raypenver <raypenver101@outlook.com>
2023-12-11 12:17:37 +0000

    got images and added links

--- src/data/wordlist.js
+++ src/data/wordlist.js
@@ -1,71 +1,87 @@
 export const words = [
-	{
-		word: "Cat",
-		image: "cat.jpeg",
-	},
-	{
-		word: "Dog",
-		image: "cat.jpeg",
-	},
+  {
+    word: "Cat",
+    image:
+      "https://i.etsystatic.com/35554607/r/il/699171/4692345180/il_1080xN.4692345180_jsca.jpg",
+  },
+  {
+    word: "Dog",
+    image:
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/6007/dog-clipart-md.png",
+  },
 
-	{
-		word: "Boy",
-		image: "boy.jpeg",
-	},
-	{
-		word: "Bus",
-		image: "bus.jpeg",
-	},
+  {
+    word: "Boy",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/015/840/578/non_2x/cartoon-cute-boy-waving-hand-free-vector.jpg",
+  },
+  {
+    word: "Bus",
+    image:
+      "https://img.freepik.com/free-vector/cartoon-school-bus-white-background_1308-132853.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1702252800&semt=ais",
+  },
 
-	{
-		word: "Bee",
-		image: "bee.jpeg",
-	},
+  {
+    word: "Bee",
+    image:
+      "https://i.etsystatic.com/13434992/r/il/3bf90f/3219303725/il_fullxfull.3219303725_kovf.jpg",
+  },
 
-	{
-		word: "Cow",
-		image: "cow.jpeg",
-	},
-	{
-		word: "Dad",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Egg",
-		image: "egg.jpeg",
-	},
-	{
-		word: "Fox",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Hat",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Mum",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Car",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Key",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Pig",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Ear",
-		image: "dad.jpeg",
-	},
-	{
-		word: "Owl",
-		image: "dad.jpeg",
-	},
+  {
+    word: "Cow",
+    image:
+      "https://t4.ftcdn.net/jpg/02/58/94/67/360_F_258946740_dBbJY1rhi3x46WZHgkrbf9OxZ1wJiP8B.jpg",
+  },
+  {
+    word: "Dad",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/025/376/273/non_2x/father-carrying-his-son-cartoon-colored-clipart-free-vector.jpg",
+  },
+  {
+    word: "Egg",
+    image:
+      "https://www.shutterstock.com/image-vector/happy-cute-smiling-egg-vector-600nw-694413472.jpg",
+  },
+  {
+    word: "Fox",
+    image:
+      "https://img.freepik.com/premium-vector/fox-cartoon-colored-clipart-illustration_576561-1907.jpg?w=2000",
+  },
+  {
+    word: "Hat",
+    image:
+      "https://www.shutterstock.com/image-vector/hat-vector-illustrationisolated-on-white-600nw-2183045863.jpg",
+  },
+  {
+    word: "Mum",
+    image:
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/34003/mother-baby-clipart-xl.png",
+  },
+  {
+    word: "Car",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/008/957/252/original/flat-red-car-icon-clipart-in-cartoon-graphic-illustration-design-vector.jpg",
+  },
+  {
+    word: "Key",
+    image:
+      "https://static.wikia.nocookie.net/crashban/images/f/fc/Cortex-key.png/revision/latest?cb=20170729064525",
+  },
+  {
+    word: "Pig",
+    image:
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/7925257/pig-clipart-xl.png",
+  },
+  {
+    word: "Ear",
+    image:
+      "https://www.shutterstock.com/image-vector/vector-illustration-ear-on-white-600nw-1161038935.jpg",
+  },
+  {
+    word: "Owl",
+    image:
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/9643/owl-clipart-xl.png",
+  },
 ];
 
 // [Cat, Dog, Boy, Bus, Bee, Cow, Dad, Egg, Fox, Hat, Mum, Car, "Key", "Pig", "Ear", "Owl"]
